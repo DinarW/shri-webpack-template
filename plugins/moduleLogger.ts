05a3fe8b11ef160bb4c4e51fba122ce74d6f782d
DinarW <dinarmkbg@gmail.com>
2022-11-06 21:48:05 +0300

    feature: module logger, search unused modules

--- plugins/moduleLogger.ts
+++ plugins/moduleLogger.ts
@@ -1,20 +1,59 @@
 import { Compiler } from 'webpack';
+import { join } from 'path';
+import fsp from 'fs/promises';
 
 class ModuleLogger {
-    apply(compiler: Compiler) {
-        compiler.hooks.normalModuleFactory.tap(
-            'ModuleLogger',
-            (normalModuleFactory) => {
-                normalModuleFactory.hooks.module.tap('ModuleLogger', (_module, _createData, resolveData) => {
-                    // @ts-ignore
-                    console.log(_createData.resource);
+    whiteList: RegExp[]
 
-                    console.log(resolveData.context);
+    constructor() {
+        this.whiteList = [
+            /.*node_modules.*/,
+            /.*plugins.*/,
+            /\.git.*$/,
+            /\.idea.*$/,
+            /\.json$/,
+            /\.nvmrc$/,
+            /\.config\.[tj]s$/,
+            /\.prettierrc\.yaml$/,
+            /README\.md$/,
+            /index\.html$/,
+            /.*utils\/whiteList.ts/
+        ]
+    }
 
-                    return _module;
-                });
+    whiteListHas(path: string) {
+        return this.whiteList.some(rgx => rgx.test(path));
+    }
+
+    async getModules(currentFolderPath: string): Promise<string[]> {
+        const entities = await fsp.readdir(currentFolderPath, { withFileTypes: true });
+        let modules: string[] = [];
+
+        for (const entity of entities) {
+            const name = entity.name;
+            const isDirectory = entity.isDirectory();
+            const currentPath = join(currentFolderPath, name);
+
+            if (!this.whiteListHas(currentPath)) {
+                if (isDirectory) {
+                    modules = modules.concat(...(await this.getModules(currentPath)))
+                } else {
+                    modules.push(currentPath);
+                }
             }
-        );
+        }
+
+        return modules;
+    }
+
+    apply(compiler: Compiler) {
+        compiler.hooks.emit.tapPromise(ModuleLogger.name, async (compilation) => {
+            const compileModules = new Set(Array.from(compilation.fileDependencies));
+            const modulesProject = await this.getModules(compiler.context);
+
+            const unused = modulesProject.filter(module => !compileModules.has(module))
+            await fsp.writeFile('unused.json', JSON.stringify(unused));
+        });
     }
 }
 
